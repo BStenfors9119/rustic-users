55067ee7bfd10ce8b5e2ec7fcf2b43b8ff2b34cc
BStenfors9119 <bstenfors@sodapopsystems.com>
2020-01-07 22:16:14 -0800

    Initial commit

---
+++ src/rustic_grpc.rs
@@ -0,0 +1,127 @@
+// This file is generated. Do not edit
+// @generated
+
+// https://github.com/Manishearth/rust-clippy/issues/702
+#![allow(unknown_lints)]
+#![allow(clippy)]
+
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+#![allow(box_pointers)]
+#![allow(dead_code)]
+#![allow(missing_docs)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(trivial_casts)]
+#![allow(unsafe_code)]
+#![allow(unused_imports)]
+#![allow(unused_results)]
+
+
+// server interface
+
+pub trait UserService {
+    fn get_users(&self, o: ::grpc::ServerHandlerContext, req: ::grpc::ServerRequestSingle<super::rustic::GetUsersRequest>, resp: ::grpc::ServerResponseUnarySink<super::rustic::GetUsersResponse>) -> ::grpc::Result<()>;
+
+    fn get_user_by_email(&self, o: ::grpc::ServerHandlerContext, req: ::grpc::ServerRequestSingle<super::rustic::GetUserByEmailRequest>, resp: ::grpc::ServerResponseUnarySink<super::rustic::GetUserResponse>) -> ::grpc::Result<()>;
+
+    fn get_user_github_info(&self, o: ::grpc::ServerHandlerContext, req: ::grpc::ServerRequestSingle<super::rustic::GetUserGithubInfoRequest>, resp: ::grpc::ServerResponseUnarySink<super::rustic::GetUserGithubInfoResponse>) -> ::grpc::Result<()>;
+}
+
+// client
+
+pub struct UserServiceClient {
+    grpc_client: ::std::sync::Arc<::grpc::Client>,
+}
+
+impl ::grpc::ClientStub for UserServiceClient {
+    fn with_client(grpc_client: ::std::sync::Arc<::grpc::Client>) -> Self {
+        UserServiceClient {
+            grpc_client: grpc_client,
+        }
+    }
+}
+
+impl UserServiceClient {
+    pub fn get_users(&self, o: ::grpc::RequestOptions, req: super::rustic::GetUsersRequest) -> ::grpc::SingleResponse<super::rustic::GetUsersResponse> {
+        let descriptor = ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+            name: ::grpc::rt::StringOrStatic::Static("/users.UserService/get_users"),
+            streaming: ::grpc::rt::GrpcStreaming::Unary,
+            req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+            resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+        });
+        self.grpc_client.call_unary(o, req, descriptor)
+    }
+
+    pub fn get_user_by_email(&self, o: ::grpc::RequestOptions, req: super::rustic::GetUserByEmailRequest) -> ::grpc::SingleResponse<super::rustic::GetUserResponse> {
+        let descriptor = ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+            name: ::grpc::rt::StringOrStatic::Static("/users.UserService/get_user_by_email"),
+            streaming: ::grpc::rt::GrpcStreaming::Unary,
+            req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+            resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+        });
+        self.grpc_client.call_unary(o, req, descriptor)
+    }
+
+    pub fn get_user_github_info(&self, o: ::grpc::RequestOptions, req: super::rustic::GetUserGithubInfoRequest) -> ::grpc::SingleResponse<super::rustic::GetUserGithubInfoResponse> {
+        let descriptor = ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+            name: ::grpc::rt::StringOrStatic::Static("/users.UserService/get_user_github_info"),
+            streaming: ::grpc::rt::GrpcStreaming::Unary,
+            req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+            resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+        });
+        self.grpc_client.call_unary(o, req, descriptor)
+    }
+}
+
+// server
+
+pub struct UserServiceServer;
+
+
+impl UserServiceServer {
+    pub fn new_service_def<H : UserService + 'static + Sync + Send + 'static>(handler: H) -> ::grpc::rt::ServerServiceDefinition {
+        let handler_arc = ::std::sync::Arc::new(handler);
+        ::grpc::rt::ServerServiceDefinition::new("/users.UserService",
+            vec![
+                ::grpc::rt::ServerMethod::new(
+                    ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+                        name: ::grpc::rt::StringOrStatic::Static("/users.UserService/get_users"),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                        resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |ctx, req, resp| (*handler_copy).get_users(ctx, req, resp))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+                        name: ::grpc::rt::StringOrStatic::Static("/users.UserService/get_user_by_email"),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                        resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |ctx, req, resp| (*handler_copy).get_user_by_email(ctx, req, resp))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::grpc::rt::ArcOrStatic::Static(&::grpc::rt::MethodDescriptor {
+                        name: ::grpc::rt::StringOrStatic::Static("/users.UserService/get_user_github_info"),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                        resp_marshaller: ::grpc::rt::ArcOrStatic::Static(&::grpc_protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |ctx, req, resp| (*handler_copy).get_user_github_info(ctx, req, resp))
+                    },
+                ),
+            ],
+        )
+    }
+}
